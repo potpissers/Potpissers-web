45244d10f1f09ea55f003426c43f5c071ed09704
Cameron <cuckmeron@gmail.com>
2025-01-21 16:53:49 -0500

    .

--- main.go
+++ main.go
@@ -20,5 +20,9 @@ func main() {
 	// http.HandleFunc("mz", func(w http.ResponseWriter, r *http.Request) {
 	// 	http.ServeFile(w, r, "mz.html")
 	// })
+
+	http.Handle("/style.css", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	http.Handle("/potpisser.jpg", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
 	http.ListenAndServe(":8080", nil)
 }
