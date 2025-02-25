448c66f8ba710316c6881aa45be486b2fd3568c7
cameron <cuckmeron@gmail.com>
2025-02-24 22:07:19 -0500

    .

--- static.js
+++ static.js
@@ -2,3 +2,6 @@ function doUnhide(id) {
     const tips = document.getElementById(id);
     tips.hidden=!tips.hidden;
 }
+function doScrollTop(id) {
+    document.getElementById('deaths').scrollTop = 0;
+}
