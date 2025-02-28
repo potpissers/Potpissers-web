61c11cb4b0b58f3ba847412068b9507f94371cf2
cameron <cuckmeron@gmail.com>
2025-02-28 00:52:42 -0500

    .

--- static.js
+++ static.js
@@ -1,7 +1,11 @@
 function handleTipsButtonClick(otherButtonsIds, clickedId) {
     for (let buttonId of otherButtonsIds) {
-        document.getElementById(buttonId).hidden = true;
+        document.getElementById(buttonId).hidden = true
     }
 
-    document.getElementById(clickedId).hidden = false;
+    document.getElementById(clickedId).hidden = false
+}
+
+function handleContentMaximizeButtonClick(isAnnouncements) {
+    document.getElementById("content").style.gridTemplateRows = isAnnouncements ? "auto 1fr" : "1fr auto"
 }
