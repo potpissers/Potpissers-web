40f650d42676394b49d13f9f1db8d7f259334870
cameron <cuckmeron@gmail.com>
2025-03-25 13:00:11 -0400

    .

--- static.js
+++ static.js
@@ -7,11 +7,17 @@ function handleTipsButtonClick(otherButtonsIds, clickedId) {
     document.getElementById(clickedId).hidden = false
 }
 
+const clickedLineItemClasses = new Set()
 function handleClassHiddenToggle(className) {
     const elements = document.getElementsByClassName(className)
     const newValue = !elements[0].hidden
     for (let i = 0; i < elements.length; i++)
         elements[i].hidden = newValue
+
+    if (!newValue)
+        clickedLineItemClasses.add(className)
+    else
+        clickedLineItemClasses.delete(className)
 }
 
 let nameCheckController = null;
@@ -47,7 +53,10 @@ function handleContentMaximizeButtonClick(isAnnouncements) {
         if (contentElement.style.gridTemplateRows === "auto 22vh") {
             contentElement.style.gridTemplateRows = "1fr 1fr"
             document.body.style.gridTemplateRows = "44vh 44vh auto"
-            // TODO -> reset line item classes
+
+            for (let className of clickedLineItemClasses)
+                handleClassHiddenToggle(className)
+            clickedLineItemClasses.clear()
         } else {
             contentElement.style.gridTemplateRows = "auto 22vh"
             document.body.style.gridTemplateRows = "44vh auto auto"
