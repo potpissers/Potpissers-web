2e28d0d6d30083768598de802ba5d510f5033c10
cameron <cuckmeron@gmail.com>
2025-03-26 00:24:21 -0400

    asd

--- main-init.go
+++ main-init.go
@@ -100,7 +100,7 @@ var deaths = func() []death {
 	var deaths []death
 	getRowsBlocking("SELECT * FROM get_12_latest_network_deaths()", func(rows pgx.Rows) {
 		var death death
-		handleFatalPgx(pgx.ForEachRow(rows, []any{&death.ServerName, &death.VictimUserFightId, &death.Timestamp, death.VictimUuid, nil, &death.DeathWorldName, &death.DeathX, &death.DeathY, &death.DeathZ, &death.DeathMessage, death.KillerUuid, nil, nil}, func() error {
+		handleFatalPgx(pgx.ForEachRow(rows, []any{&death.ServerName, death.VictimUserFightId, &death.Timestamp, &death.VictimUuid, nil, &death.DeathWorldName, &death.DeathX, &death.DeathY, &death.DeathZ, &death.DeathMessage, death.KillerUuid, nil, nil}, func() error {
 			deaths = append(deaths, death)
 			return nil
 		}))
