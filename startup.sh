83456386369d79cbd3c4966f569bf4eceedeefe2
cameron <cuckmeron@gmail.com>
2025-04-10 01:39:13 -0400

    asd

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 while true; do
     git fetch upstream
     git reset --hard upstream/main
-    ./Potpissers-web
+    /Potpissers-web/./Potpissers-web
     echo Server restarting...
     echo Press CTRL + C to stop.
 done
