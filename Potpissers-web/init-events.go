6a4f29dacfc18ef2aa055103a73ec46fb65b516d
cameron <cuckmeron@gmail.com>
2025-04-12 15:55:48 -0400

    asd

--- Potpissers-web/init-events.go
+++ Potpissers-web/init-events.go
@@ -7,26 +7,27 @@ type abstractEvent struct {
 	Timestamp time.Time
 }
 
-var events = func() []abstractEvent {
-	var events []abstractEvent
-	i, j := 0, 0
-	for i < len(koths) && j < len(supplyDrops) {
-		koth, drop := koths[i], supplyDrops[j]
-		if koth.EndTimestamp.After(drop.EndTimestamp) {
-			events = append(events, abstractEvent{koth.CapMessage, koth.EndTimestamp})
-			i++
-		} else {
-			events = append(events, abstractEvent{drop.WinMessage, drop.EndTimestamp})
-			j++
-		}
-	}
-	for i < len(koths) {
-		koth := koths[i]
-		events = append(events, abstractEvent{koth.CapMessage, koth.EndTimestamp})
-	}
-	for j < len(supplyDrops) {
-		drop := supplyDrops[j]
-		events = append(events, abstractEvent{drop.WinMessage, drop.EndTimestamp})
-	}
-	return events
-}()
+var events []abstractEvent
+// var events = func() []abstractEvent {
+// 	var events []abstractEvent
+// 	i, j := 0, 0
+// 	for i < len(koths) && j < len(supplyDrops) {
+// 		koth, drop := koths[i], supplyDrops[j]
+// 		if koth.EndTimestamp.After(drop.EndTimestamp) {
+// 			events = append(events, abstractEvent{koth.CapMessage, koth.EndTimestamp})
+// 			i++
+// 		} else {
+// 			events = append(events, abstractEvent{drop.WinMessage, drop.EndTimestamp})
+// 			j++
+// 		}
+// 	}
+// 	for i < len(koths) {
+// 		koth := koths[i]
+// 		events = append(events, abstractEvent{koth.CapMessage, koth.EndTimestamp})
+// 	}
+// 	for j < len(supplyDrops) {
+// 		drop := supplyDrops[j]
+// 		events = append(events, abstractEvent{drop.WinMessage, drop.EndTimestamp})
+// 	}
+// 	return events
+// }()
