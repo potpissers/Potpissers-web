65dfcbdf26d6206cf505bd3696ff88c3c7886967
cameron <cuckmeron@gmail.com>
2025-04-16 00:07:12 -0400

    .

--- Potpissers-web/frontend/static-donate.js
+++ Potpissers-web/frontend/static-donate.js
@@ -4,16 +4,15 @@ const currentLineItemsLowercaseUsernames = new Set()
 const currentLineItemsUsernameCheckPromises = []
 let currentLineItemsCost = 0
 
-function handleAddLineItemJson(itemName, itemAmountString) {
-    const username = document.getElementById("donate-username").value
-    if (username.length === 0)
-        return alert("invalid username: " + username)
+function handleAddLineItemJson(usernameInputString, itemName, itemAmountString) {
+    if (usernameInputString.length === 0)
+        return alert("invalid username: " + usernameInputString)
     else {
         currentLineItemsCost += currentPrices[itemName] * parseInt(itemAmountString)
         document.getElementById("checkoutbalance").innerText = "$" + currentLineItemsCost / 100
 
         privateJsonLineItems.push({
-            username: username,
+            username: usernameInputString,
             line_item_name: itemName,
             line_item_amount: parseInt(itemAmountString, 10),
         })
@@ -23,9 +22,9 @@ function handleAddLineItemJson(itemName, itemAmountString) {
         document.getElementById("donatesidebutton").hidden = true
         document.getElementById("donatesidebuttonred").hidden = false
 
-        if (!currentLineItemsLowercaseUsernames.has(username.toLowerCase())) {
-            currentLineItemsLowercaseUsernames.add(username.toLowerCase())
-            handleUsernameApiCheck(username)
+        if (!currentLineItemsLowercaseUsernames.has(usernameInputString.toLowerCase())) {
+            currentLineItemsLowercaseUsernames.add(usernameInputString.toLowerCase())
+            handleUsernameApiCheck(usernameInputString)
         }
     }
 }
