ca84d238fb5f50dfabe07093f2f810864ecc51f5
cameron <cuckmeron@gmail.com>
2025-09-02 18:15:30 -0400

    .

--- Potpissers-web/startup.sh
+++ Potpissers-web/startup.sh
@@ -3,7 +3,7 @@
 while true; do
     git fetch origin
     git reset --hard origin/main
-    Potpissers-web/./Potpissers-web
+    ./Potpissers-web
     echo Server restarting...
     echo Press CTRL + C to stop.
 done
